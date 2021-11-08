04f761f1a8017623af7042ee4b58aba4b610b556
Brian Bycynski <high5bri@hotmail.com>
2021-11-08 02:34:26 -0500

    React Portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 //
 //
 
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
 function App() {
   return (
-    <div>
-      <h2>Let's fucking go!</h2>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
