0191d63cf87e6d9b0c9a0a2d9088d96a1233d4a9
Brian Bycynski <high5bri@hotmail.com>
2021-11-10 03:09:48 -0500

    Adding a Form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,7 +2,9 @@
 //
 
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -33,12 +35,22 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li key={meal.id}>{meal.name}</li>;
+    // return <li key={meal.id}>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,6 +1,21 @@
 //
 //
 
+import React from 'react';
+
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <input type='text' />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
+
 // import { useRef, useState } from 'react';
 
 // import Input from '../../UI/Input';
